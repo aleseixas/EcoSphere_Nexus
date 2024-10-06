7cc601334d8932db637b507174721c723bded56c
aleseixas <alelseixas@hotmail.com>
2024-10-06 20:26:34 -0300

    criando pagina de biomas

--- nasaFrontEnd/src/App.tsx
+++ nasaFrontEnd/src/App.tsx
@@ -1,14 +1,28 @@
 import './App.css';
-import MapComponent from './components/MapComponent';
-import Sidebar from './components/SideBar'; // Importa o Sidebar
+import './components/EcosystemProfile'
+// import MapComponent from './components/MapComponent';
+// import Sidebar from './components/SideBar'; // Importa o Sidebar
 import 'leaflet/dist/leaflet.css';
+import EcosystemProfile from './components/EcosystemProfile';
+import Info from './pages/Info';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* Adiciona o Sidebar ao lado do Mapa */}
+//       <Sidebar />
+//       <MapComponent />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
     <div className="App">
       {/* Adiciona o Sidebar ao lado do Mapa */}
-      <Sidebar />
-      <MapComponent />
+      <Info />
     </div>
   );
 }
---
+++ nasaFrontEnd/src/pages/Info.tsx
@@ -0,0 +1,79 @@
+import './../App.css';
+import './Info.css';
+import tundraImage from './../assets/tundra.jpeg';
+import savannahImage from './../assets/savana.jpeg';
+import tropicalForestImage from './../assets/floresta_tropical.jpeg';
+import temperateForestImage from './../assets/floresta_temperada.jpeg';
+import desertImage from './../assets/deserto.jpeg';
+import mangroveImage from './../assets/manguezal.jpeg';
+import prairieImage from './../assets/pradaria.jpeg';
+import oceanImage from './../assets/oceano.jpeg';
+import coralImage from './../assets/coral.jpeg';
+import 'leaflet/dist/leaflet.css';
+import EcosystemProfile from '../components/EcosystemProfile';
+
+
+const ecosystems = [
+    {
+        name: 'Tundras',
+        image: tundraImage,
+        description: 'Tundras are cold, dry regions with frozen soil and limited vegetation. They help regulate the global climate and store large amounts of carbon in the soil. They affect temperate forests and oceans due to their impact on the global climate.',
+    },
+    {
+        name: 'Savannahs',
+        image: savannahImage,
+        description: 'Savannahs are open areas with grasses and few trees, located in warm regions. They are habitats for large mammals and influence prairies and deserts, especially with changes in the rainfall regime.',
+    },
+    {
+        name: 'Temperate Forests',
+        image: temperateForestImage,
+        description: 'Temperate forests have four distinct seasons and trees that lose their leaves in the fall. They produce oxygen and store carbon, affecting tundras and prairies by regulating temperature and climate.',
+    },
+    {
+        name: 'Tropical Forests',
+        image: tropicalForestImage,
+        description: 'Tropical forests are warm and humid ecosystems that host a vast biodiversity. They are essential for oxygen production and carbon absorption. They influence mangroves and oceans by providing nutrients to these ecosystems.',
+    },
+    {
+        name: 'Deserts',
+        image: desertImage,
+        description: 'Deserts are dry areas with little precipitation and vegetation adapted to aridity. They regulate Earth’s temperatures and can influence savannahs and prairies with the advance of desertification.',
+    },
+    {
+        name: 'Prairies',
+        image: prairieImage,
+        description: 'Prairies are flat areas covered with grasses, experiencing cold winters and hot summers. They are important for agriculture and affect temperate forests and savannahs, especially with climate changes.',
+    },
+    {
+        name: 'Mangroves',
+        image: mangroveImage,
+        description: 'Mangroves are coastal forests that grow in salt or brackish water. They protect coastlines from erosion and are nurseries for many marine species. They influence coral reefs and oceans with their nutrients.',
+    },
+    {
+        name: 'Coral Reefs',
+        image: coralImage,
+        description: 'Coral reefs are underwater formations made by corals, hosting a large diversity of marine life. They protect coastlines from erosion and affect mangroves and oceans with their biodiversity.',
+    },
+    {
+        name: 'Oceans',
+        image: oceanImage,
+        description: 'Oceans cover more than 70% of the Earth, regulate the global climate, and produce most of the oxygen we breathe. They influence coral reefs, mangroves, and tropical forests by transporting nutrients and heat.',
+    },
+];
+
+function Info() {
+  return (
+    <div className="Info">
+        {ecosystems.map((item) => (
+          <EcosystemProfile 
+            className={`ecosystem-profile ${item.name.replace(/\s+/g, '')}`} 
+            name={item.name}
+            image={item.image}
+            description={item.description}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default Info;
